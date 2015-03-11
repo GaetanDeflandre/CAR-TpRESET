96208249ec53e78b87b0b303ff5545d351b9d49f
Gaëtan DEFLANDRE <gaetan.deflandre@gmail.com>
2015-03-11 01:41:13 +0100

    changeDir considéré par le serveur FTP, apres on quit le serveur, donc le serveur que l'on ouvre ensuite n'est pas sur le bon chemin

--- src/main/java/res/DirResource.java
+++ src/main/java/res/DirResource.java
@@ -2,11 +2,15 @@ package res;
 
 import java.io.IOException;
 import java.net.SocketException;
+import java.net.URI;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
@@ -24,9 +28,11 @@ public class DirResource {
 	public final static String ADDRESS = "127.0.0.1";
 	public final static int PORT = 2121;
 	public final static String ENDL = "\r\n";
-	
+
 	public final static String LOGIN = "toto";
 	public final static String PASS = "mdp";
+	public final static String RES_ABS_PATH = "http://localhost:8080/rest/api/dir";
+	public final static String RES_ROOT = "dir";
 
 	@GET
 	@Produces("text/html")
@@ -54,19 +60,26 @@ public class DirResource {
 			html += e.getMessage() + ENDL;
 			return html;
 		}
-		
+
 		// LIST
 		try {
 			FTPFile[] files = client.listFiles();
-			
+
 			html += "<ul>" + ENDL;
 			for (FTPFile file : files) {
-				if(file.isDirectory()){
-					html += "<li>" + file.getName() + "</li>";
-				} else if(file.isFile()){
-					html += "<li>" + file.getName() + "</li>";
+				if (file.isDirectory()) {
+					html += "<li><a href=" + RES_ABS_PATH + "/"
+							+ file.getName() + ">";
+					html += file.getName();
+					html += "</a></li>" + ENDL;
+
+				} else if (file.isFile()) {
+					html += "<li><a href=#>";
+					html += file.getName();
+					html += "</a></li>" + ENDL;
+
 				} else {
-					html += "<li>" + file.getName() + "</li>";
+					html += "<li>" + file.getName() + "</li>" + ENDL;
 				}
 			}
 			html += "</ul>" + ENDL;
@@ -75,7 +88,7 @@ public class DirResource {
 			html += e.getMessage() + ENDL;
 			return html;
 		}
-		
+
 		// QUIT
 		try {
 			client.quit();
@@ -88,11 +101,69 @@ public class DirResource {
 		return html;
 	}
 
+	/*
+	 * /!\ README 
+	 * La methode change dir est fonctionelle: elle fait le CWD coté
+	 * serveur mais comme on quit le serveur, le chemin n'est pas 
+	 * retenu coté serveur
+	 */
+
+	/**
+	 * Change directory
+	 * 
+	 * @param uriInfo
+	 * @param dirName
+	 * @return
+	 */
 	@GET
-	@Path("a/{isbn}")
-	@Produces("text/html")
-	public String getBook(@PathParam("isbn") String isbn) {
-		return "Book: " + isbn;
+	@Path("/{dirname}")
+	public Response changeDir(@Context UriInfo uriInfo,
+			@PathParam("dirname") String dirName) {
+
+		Response res;
+		URI uri;
+
+		FTPClient client = new FTPClient();
+
+		// CONNECT
+		try {
+			client.connect(ADDRESS, PORT);
+		} catch (SocketException e) {
+			res = Response.serverError().build();
+			return res;
+		} catch (IOException e) {
+			res = Response.serverError().build();
+			return res;
+		}
+
+		// LOG
+		try {
+			client.login(LOGIN, PASS);
+		} catch (IOException e) {
+			res = Response.serverError().build();
+			return res;
+		}
+
+		// CHANGE DIR
+		try {
+			client.changeWorkingDirectory(dirName);
+		} catch (IOException e) {
+			res = Response.serverError().build();
+			return res;
+		}
+
+		// QUIT
+		try {
+			client.quit();
+			client.disconnect();
+		} catch (IOException e) {
+			res = Response.serverError().build();
+			return res;
+		}
+
+		uri = uriInfo.getBaseUriBuilder().path(RES_ROOT).build();
+		res = Response.seeOther(uri).build();
+		return res;
 	}
 
 	@GET
