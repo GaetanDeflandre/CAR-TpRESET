3db21362dc019996866983f21b864fa67a511a75
GaetanDeflandre <gaetan.deflandre@gmail.com>
2015-03-11 12:16:32 +0100

    ajout du package utils avec des constantes

---
+++ src/main/java/utils/HtmlUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ * Classe contenant des constantes utilent pour le HTML
+ */
+public class HtmlUtils {
+
+	/**
+	 * Chaine contenant les caractères permettant le retour chariot.
+	 */
+	public final static String ENDL = "\r\n";
+
+}
