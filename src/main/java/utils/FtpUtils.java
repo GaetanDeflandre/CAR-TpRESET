73cac0b57c986e0c67700ade3952bb24b2c8f9ad
Gaëtan DEFLANDRE <gaetan.deflandre@gmail.com>
2015-03-19 00:08:16 +0100

    ajout de doc FtpUtils

--- src/main/java/utils/FtpUtils.java
+++ src/main/java/utils/FtpUtils.java
@@ -1,8 +1,18 @@
 package utils;
 
+/**
+ * Information sur le serveur FTP.
+ */
 public class FtpUtils {
 	
+	/**
+	 * Adresse du serveur FTP.
+	 */
 	public final static String ADDRESS = "127.0.0.1";
+	
+	/**
+	 * Port de communication du serveur FTP.
+	 */
 	public final static int PORT = 2121;
 
 	public final static String LOGIN = "toto";
